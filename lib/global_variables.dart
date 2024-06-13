2a0f259a2093c68eac6076143e9cfe28742df4bf
Vishal KOchiyaniya <147090166+vkochiyaniya@users.noreply.github.com>
2024-06-13 18:14:31 +0530

    first commit

---
+++ lib/global_variables.dart
@@ -0,0 +1,33 @@
+final products = [{
+  'id': '0',
+  'title': 'Men\'s Nike Shoes',
+  'price': 66.75,
+  'sizes': [8, 9, 10, 11],
+  'company': 'Nike',
+  'imageUrl': 'asssets/images/shoes_1.png',
+},
+{
+  'id': '1',
+  'title': 'Men\'s Nike Shoes',
+  'price': 66.75,
+  'sizes': [8, 9, 10, 11, 12],
+  'company': 'Addidas',
+  'imageUrl': 'asssets/images/shoes_2.png',
+},
+{
+  'id': '2',
+  'title': 'Men\'s Nike Shoes',
+  'price': 66.75,
+  'sizes': [8, 9, 10, 11],
+  'company': 'Bata',
+  'imageUrl': 'asssets/images/shoes_3.png',
+},
+{
+  'id': '3',
+  'title': 'Men\'s Nike Shoes',
+  'price': 66.75,
+  'sizes': [9, 10, 11],
+  'company': 'Nike',
+  'imageUrl': 'asssets/images/shoes_4.png',
+}
+];
